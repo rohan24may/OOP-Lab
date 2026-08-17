d4144129a8a5defcbf06ef3484833ced24fd8ec5
Rohan Hudati <rohanabcd0305@gmail.com>
2026-08-17 12:35:41 +0530

    Add basic I/O examples in Q1).cpp

---
+++ Q1).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello\nWorld";          // New line
+
+    cout << "\nHello\tWorld";        // Tab
+
+    cout << "\nAlarm\a";              // Alarm sound
+
+    cout << "\n12345\rABC";           // Carriage return
+
+    return 0;
+}
